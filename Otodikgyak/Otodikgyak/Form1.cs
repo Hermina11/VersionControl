870d5bab631db28a15824c28f5cdb219d6cd848d
hermina <hermina.acsai@stud.uni-corvinus.hu>
2022-10-21 11:11:17 +0200

    6

--- Otodikgyak/Otodikgyak/Form1.cs
+++ Otodikgyak/Otodikgyak/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace Otodikgyak
@@ -68,6 +69,24 @@ namespace Otodikgyak
             }
 
 
+        }
+
+        private void Diagram() {
+            chartRateData.DataSource = Rates; //adatforráas a Rates lista
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
     
