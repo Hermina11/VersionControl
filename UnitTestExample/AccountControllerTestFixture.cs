962717eab77dc730059acdc1fe550d04399eec45
hermina <hermina.acsai@stud.uni-corvinus.hu>
2022-11-25 10:51:34 +0100

    E-mail validálás teszt kifejtése

--- UnitTestExample/AccountControllerTestFixture.cs
+++ UnitTestExample/AccountControllerTestFixture.cs
@@ -4,14 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample
 {
     class AccountControllerTestFixture
     {
         [Test]
-        public void TestValidateEmail (string eamil, bool expectedResult)
+        public void TestValidateEmail (string email, bool expectedResult)
         {
+            var accountController = new AccountController();
+            var actualResult = accountController.ValidateEmail(email);
+            Assert.AreEqual(actualResult, expectedResult);
         }
     }
 }
